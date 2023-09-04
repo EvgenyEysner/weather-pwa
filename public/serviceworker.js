bfe94490ff2aa86dc3ef35377c7ea009392d7792
Evgeny Eysner <evgeny.eysner@gmail.com>
2023-09-04 17:47:17 +0200

    feat: add config files

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -3,40 +3,39 @@ const urlsToCache = ["index.html", "offline.html"];
 const self = this;
 
 // Install ServiceWorker
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                console.log('Opened cache');
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
 
-                return cache.addAll(urlsToCache);
-            })
-    )
+      return cache.addAll(urlsToCache);
+    }),
+  );
 });
 
 // Listen for requests
 self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        caches.match(event.request)
-        .then(() => {
-            return fetch(event.request)
-            .catch(() => caches.match("offline.html"))  
-        })
-    )
+  event.respondWith(
+    caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() => caches.match("offline.html"));
+    }),
+  );
 });
 
 // Aktivate the ServiceWorker
 self.addEventListener("activate", (event) => {
-    const cacheWihteList = [];
-    cacheWihteList.push(CACHE_NAME);
+  const cacheWihteList = [];
+  cacheWihteList.push(CACHE_NAME);
 
-    event.waitUntil(
-        caches.keys().then((cacheNames) => Promise.all(
-            cacheNames.map((cacheName) => {
-                if(!cacheWihteList.includes(cacheName)){
-                    return caches.delete(cacheName);
-                }
-            })
-        )
-    ))
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWihteList.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        }),
+      ),
+    ),
+  );
 });
